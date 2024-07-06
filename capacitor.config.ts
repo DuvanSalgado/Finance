cc41c0273a4fc41ca4d7d45e2a81c2277fab0387
Duvan Salgado <dsalgado012@gmail.com>
2024-07-05 22:27:55 -0500

    creacion de la aplicacion

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.finance',
+  appName: 'finance',
+  webDir: 'www/browser'
+};
+
+export default config;
